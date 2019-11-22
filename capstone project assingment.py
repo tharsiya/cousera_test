43f0b6de29c7405c443aa2dd0795405ee95f12a9
tharsiya <sunitikezia18@gmail.com>
2019-11-22 15:25:29 +0530

    Add files via upload
    Python file uploaded

---
+++ capstone project assingment.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### The below project is  used for Capstone 1st assignment project .In this we are going to import Pandas and Numpy library and print "Hello Capstone Project Course "
+# 
+
+# In[6]:
+
+
+import pandas  as pd
+import numpy as NP
+
+
+# In[8]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
